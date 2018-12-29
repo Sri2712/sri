2fee3c82be375386ff4e9cf875bd65523b9e29b9
unknown <rohiniamanchi@gmail.com>
2018-12-29 12:33:34 +0530

    hi

---
+++ src/org/sample/sam.java
@@ -0,0 +1,13 @@
+package org.sample;
+
+public class sam {
+private void show() {
+	// TODO Auto-generated method stub
+
+	System.out.println("`1234");
+	}
+	public static void main(String[] args) {
+		sam s=new sam();
+		s.show();
+	}
+}
